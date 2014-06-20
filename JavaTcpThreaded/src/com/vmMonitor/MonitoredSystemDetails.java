933618a4bcb8ab810da1fb5eda2123e51556855e
Bogdan Irimie <irimie.bogdan@outlook.com>
2014-06-20 21:27:17 +0300

    Change package name

---
+++ JavaTcpThreaded/src/com/vmMonitor/MonitoredSystemDetails.java
@@ -0,0 +1,24 @@
+package com.vmMonitor;
+
+public class MonitoredSystemDetails {
+
+    private long processors;
+    private long memory;
+
+    public long getProcessors() {
+        return processors;
+    }
+
+    public void setProcessors(long processors) {
+        this.processors = processors;
+    }
+
+    public long getMemory() {
+        return memory;
+    }
+
+    public void setMemory(long memory) {
+        this.memory = memory;
+    }
+
+}
