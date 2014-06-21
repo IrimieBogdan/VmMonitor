689adf97316a0e2700eeca88d449ac969a61fe5b
Bogdan Irimie <irimie.bogdan@outlook.com>
2014-06-21 23:14:19 +0300

    Refactor client code, add comments.

--- Client/src/Client.c
+++ Client/src/Client.c
@@ -5,56 +5,108 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * socket used to connect to server
+ */
+int sock;
+/**
+ * Structure with server data
+ */
+struct sockaddr_in server;
+
+int openConnection(char* ip, char* port);
+int sendMessage(char* message);
+int closeConnection();
 char* getSystemData();
+
 
 int main(int argc , char* argv[])
 {
-    int sock, iResult;
-    struct sockaddr_in server;
-    char* message;
+    char* systemDetails;
+    systemDetails = getSystemData();
+    openConnection(argv[1], argv[2]);
+    sendMessage(systemDetails);
+    closeConnection();
+    return 0;
+}
 
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-        return -1;
-    }
-    puts("Socket created");
+/**
+ * Open connection to server.
+ *
+ * @param ip	ip to connect to.
+ * @param port	port used for connection.
+ * @return	0 success, -1 fail to connect to server
+ */
+int openConnection(char* ip, char* port)
+{
+	int iResult;
 
-    server.sin_addr.s_addr = inet_addr(argv[1]);
-    server.sin_family = AF_INET;
-    server.sin_port = htons( atoi(argv[2]) );
+	//Create socket
+	sock = socket(AF_INET , SOCK_STREAM , 0);
+	if (sock == -1)
+	{
+		printf("Could not create socket");
+		return -1;
+	}
+	puts("Socket created");
 
-    //Connect to remote server
-    iResult = connect(sock , (struct sockaddr *)&server , sizeof(server));
-    if (iResult < 0)
-    {
-        printf("Connect failed!");
-        close(sock);
-        return -1;
-    }
-    puts("Connected\n");
+	server.sin_addr.s_addr = inet_addr(ip);
+	server.sin_family = AF_INET;
+	server.sin_port = htons( atoi(port) );
 
-    message = getSystemData();
+	//Connect to remote server
+	iResult = connect(sock , (struct sockaddr *)&server , sizeof(server));
+	if (iResult < 0)
+	{
+		printf("Connect failed!");
+		close(sock);
+		return -1;
+	}
+	puts("Connected\n");
+	return iResult;
+}
 
-    //Send data
+/**
+ * Send message over the socket.
+ *
+ * @param message	Text to send over the socket.
+ * @return 			Number of bytes send.
+ */
+int sendMessage(char* message)
+{
+	int iResult;
+
     iResult = send(sock , message , strlen(message) , 0);
 	if( iResult < 0)
 	{
 		printf("Send failed!");
-		close(sock);
+		closeConnection();
 		return -1;
 	}
 	puts("Message send!");
-    close(sock);
-    return 0;
+	return iResult;
 }
 
+/**
+ * Close socket
+ *
+ * @return	0 success, -1 fail to connect to server
+ */
+int closeConnection()
+{
+	return close(sock);
+}
+
+/**
+ * Get cpu count and memory and create a string in Json format.
+ *
+ * @return 	a string in Json format with system details.
+ */
 char* getSystemData()
 {
 	long processorsOnline = -1;
 	unsigned long usableMemory, t1, t2;
+	char* buf;
 
 	processorsOnline = sysconf(_SC_NPROCESSORS_ONLN);
 	t1 = get_phys_pages();
@@ -63,12 +115,10 @@ char* getSystemData()
 
 	const int length = snprintf(NULL, 0, "%ld", processorsOnline);
 	const int length2 = snprintf(NULL, 0, "%lu", usableMemory);
-	char* buf;
 	const int length3 = strlen("{\"processors\":,\"memory\":}");
 
 	buf = malloc(length + length2 + length3 + 1);
 	snprintf(buf, length + length2 + + length3 + 1, "{\"processors\":%ld,\"memory\":%lu}", processorsOnline, usableMemory);
 
 	return buf;
-
 }
